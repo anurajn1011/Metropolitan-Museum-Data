151f226ed9a81cb0a3fcf6af8571c50130b3ba9f
anurajn1011 <anurajnair2001@gmail.com>
2025-11-19 17:11:02 -0600

    Changes to sql tests, verifying consistency

--- tests/sqlite_tests.py
+++ tests/sqlite_tests.py
@@ -14,16 +14,16 @@ cursor = conn.cursor()
 # --- Queries --- #
 
 # Checking to see if all departments are indeed to be found in the db
-# print(pd.read_sql("""SELECT DISTINCT department_id FROM Objects;""", conn))
+print(pd.read_sql("""SELECT DISTINCT department_id FROM Objects;""", conn))
 
 # Checking the size of our Art table 
-# print(pd.read_sql("""SELECT COUNT(*) FROM Art""", conn))
+print(pd.read_sql("""SELECT COUNT(*) FROM Art""", conn))
 
 # checking the contents of the artists table
-# print(pd.read_sql("""SELECT artistAlphaSort FROM artists LIMIT 5""", conn))
+print(pd.read_sql("""SELECT artistAlphaSort FROM artists LIMIT 5""", conn))
 
-# # checking the end dates of objects
-# print(pd.read_sql("""SELECT title, artistAlphaSort, CAST(objectEndDate AS INTEGER) AS objectEndDate FROM Art WHERE CAST(objectEndDate AS INTEGER) > 2024""", conn))
+# checking the end dates of objects
+print(pd.read_sql("""SELECT title, artistAlphaSort, CAST(objectEndDate AS INTEGER) AS objectEndDate FROM Art WHERE CAST(objectEndDate AS INTEGER) > 2024""", conn))
 
 print(pd.read_sql("""SELECT country FROM Art LIMIT 5""", conn))
 
